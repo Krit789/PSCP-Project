7ba9ce1eba3ef459d5c23585e78cbc7ca337e7c5
“TaeTanakrit0089” <tanakritsupprasit@gmail.com>
2022-11-11 22:05:35 +0700

    Added new register page and improved css!

--- eNote/auth.py
+++ eNote/auth.py
@@ -4,16 +4,32 @@ from time import time as tme
 
 auth = Blueprint('auth', __name__)
 
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login_page():
-    rand_img = (int(str(tme()*1000)[-1])%4)+1
+    rand_img = (int(str(tme()*1000)[-1]) % 4)+1
     try:
         if request.method == 'GET':
             return render_template('login.html', bg_img=rand_img)
         if request.method == 'POST':
             if len(request.form['user']) > 0 and len(request.form['passwd']) > 0:
                 check = request.form.getlist('remember')
-                return render_template('login.html', bg_img=rand_img, data=True , var=[request.form['user'], request.form['passwd'], check])
-            return render_template('login.html', bg_img=rand_img, data=True , var1='N/A', var2='N/A')
+                return render_template('login.html', bg_img=rand_img, data=True, var=[request.form['user'], request.form['passwd'], check])
+            return render_template('login.html', bg_img=rand_img, data=True, var1='N/A', var2='N/A')
+    except TemplateNotFound:
+        abort(404)
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register_page():
+    rand_img = (int(str(tme()*1000)[-1]) % 4)+1
+    try:
+        if request.method == 'GET':
+            return render_template('register.html', bg_img=rand_img)
+        if request.method == 'POST':
+            if len(request.form['user']) > 0 and len(request.form['passwd']) > 0:
+                check = request.form.getlist('remember')
+                return render_template('register.html', bg_img=rand_img, data=True, var=[request.form['user'], request.form['passwd'], check])
+            return render_template('register.html', bg_img=rand_img, data=True, var1='N/A', var2='N/A')
     except TemplateNotFound:
         abort(404)
