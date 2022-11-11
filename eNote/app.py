dcb6266a60a49799250d9ffb0fb613eec8d86eea
unknown <jarukritsripaploen@protonmail.com>
2022-11-11 16:08:43 +0700

    Support AVIF and WebP images, randomized login background

--- eNote/app.py
+++ eNote/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-
+from time import time as tme
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,10 +8,11 @@ def hello():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    rand_img = (int(str(tme()*1000)[-1])%4)+1
     if request.method == 'GET':
-        return render_template('login.html')
+        return render_template('login.html', bg_img=rand_img)
     if request.method == 'POST':
-        return render_template('login.html',data=True , var1=request.form['user'], var2=request.form['passwd'])
+        return render_template('login.html', bg_img=rand_img, data=True , var1=request.form['user'], var2=request.form['passwd'])
 
 
 
