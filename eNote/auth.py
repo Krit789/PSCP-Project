4823dd3d75374ca0f30119e16d92a9431c15151b
“TaeTanakrit0089” <tanakritsupprasit@gmail.com>
2022-11-14 12:01:03 +0700

    Created base page

--- eNote/auth.py
+++ eNote/auth.py
@@ -9,6 +9,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
+
+@auth.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 @auth.route('/')
 def home_page():
     return render_template('home.html')
@@ -40,7 +46,7 @@ def login_page():
                 else:
                     flash('Incorrect password, please try again', category='error')
             else:
-                flash("Email or Username doesn't exist!",category='error')
+                flash("Email or Username doesn't exist!", category='error')
         return render_template("login.html", bg_img=rand_img)
     except TemplateNotFound:
         abort(404)
@@ -63,26 +69,30 @@ def register_page():
         if email_check:
             flash('Email already exist', category='error')
         elif user_check:
-            flash(f'Your username, {username} was already taken', category='error')
+            flash(
+                f'Your username, {username} was already taken', category='error')
         elif len(email) < 4:
-           flash('Email must be greater than 3 characters.', category='error')
+            flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
             flash('First name must be greater than 1 characters.', category='error')
         elif password != password_confirm:
             flash('Both passwords must be the same.', category='error')
         elif len(password) < 7:
-            flash('Password must be at least 7 characters long.', category='error') 
+            flash('Password must be at least 7 characters long.', category='error')
         else:
             if len(last_name) > 0:
-                new_user = User(username=username, email=email, first_name=first_name, last_name=last_name, password=generate_password_hash(password, method='sha384'))
+                new_user = User(username=username, email=email, first_name=first_name,
+                                last_name=last_name, password=generate_password_hash(password, method='sha384'))
             else:
-                new_user = User(username=username, email=email, first_name=first_name, password=generate_password_hash(password, method='sha384'))
+                new_user = User(username=username, email=email, first_name=first_name,
+                                password=generate_password_hash(password, method='sha384'))
             db.session.add(new_user)
             db.session.commit()
             flash('Account created!', category='success')
             return redirect(url_for('auth.login_page'))
-        
+
         return render_template('register.html', bg_img=rand_img)
+
 
 @auth.route('/logout')
 @login_required
@@ -90,6 +100,7 @@ def logout():
     logout_user()
     flash('Logged out successfully!', category='success')
     return redirect(url_for('auth.login_page'))
+
 
 @auth.route('/about')
 def about_page():
