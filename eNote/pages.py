c8cbb83bc99f4528ecdd1d5f8986eea0ad25a70e
Krit789 <jarukritsripaploen@protonmail.com>
2022-11-15 17:22:34 +0700

    Fix broken account deletion

--- eNote/pages.py
+++ eNote/pages.py
@@ -21,9 +21,10 @@ def user_profile():
         user_data = [temp.id, temp.username, temp.first_name, temp.last_name, temp.creation_date]
         return render_template("profile.html", bg_img=rand_img, user_data=user_data)
     if request.method == 'POST':
-        delete = True if request.form.get('delete') == 'on' else False
+        delete = True if request.form.get('delete') == 'Delete Account' else False
         if delete:
-            db.session.delete(User(id=current_user.id))
+            obj = User.query.filter_by(id=current_user.id).one()
+            db.session.delete(obj)
             db.session.commit()
             flash('Account deleted!', category='success')
-        return url_for('pages.home_page')
+        return redirect(url_for('pages.home_page'))
