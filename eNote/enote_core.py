9474a0056aaa47922f1f63fde1f8547f2c19cf3c
Krit789 <jarukritsripaploen@protonmail.com>
2022-11-29 02:47:08 +0700

    Major UX Improvments, Added Simple Mode adjust a lot of note menu and displays

--- eNote/enote_core.py
+++ eNote/enote_core.py
@@ -36,15 +36,20 @@ def md_cleaner(text: str) -> str:
 def note_home():
     if request.method == 'GET':
         user_note = Note.query.filter_by(user_id=current_user.id).order_by(desc(Note.last_edit))
-        return render_template("note.html", all_note=user_note, bg_img=rand_img())
+        return render_template("note.j2", all_note=user_note, bg_img=rand_img())
     if request.method == 'POST':
         action = request.form.get('action')
         if action == 'create':
-            if len(request.form.get('title')) == 0:
+            if request.form.get('title') is None and request.form.get('editor_type') == 'blank':
                 title = "Untitled Note"
+                note_content = ""
+            elif len(request.form.get('title')) == 0:
+                title = "Untitled Note"
+                note_content = md_cleaner(request.form.get('content'))
             else:
                 title = bleach.clean(request.form.get('title'))
-            memo = Note(title=title, content=md_cleaner(request.form.get('content')), user_id=current_user.id)
+                note_content = md_cleaner(request.form.get('content'))
+            memo = Note(title=title, content=note_content, user_id=current_user.id)
             db.session.add(memo)
             db.session.commit()
             flash(f'<b>{title}</b> was created successfully!', category='success')
@@ -74,13 +79,24 @@ def note_view(note_id):
     user_note = Note.query.filter_by(id=note_id).first_or_404()
     if user_note.user_id != current_user.id:
         abort(403)
-    return render_template('note_view.html', note=user_note, bg_img=rand_img())
+    return render_template('note_view.j2', note=user_note, bg_img=rand_img())
 
 @core.route('/note/edit', methods=['POST'])
 @login_required
 def editor():
+    editor_type = request.form.get('editor_type')
     note_id = request.form.get('note_id')
-    user_note = Note.query.filter_by(id=note_id).first_or_404()
-    if user_note.user_id != current_user.id:
-        abort(403)
-    return render_template('note_editor.html', note=user_note, bg_img=rand_img())
+    if note_id is None and editor_type is not None:
+        if editor_type == 'blank':
+            return redirect(url_for('core.note_home'), code=307)
+        placeholder = Note(title="", content="", id=0, creation_date="Not saved yet", last_edit="Not saved yet")
+        if editor_type in ('simple', 'full'):
+            flash("This note is a draft. <strong>Changes will not be save</strong> until you click save", category='warning')
+            return render_template('note_editor.j2', note=placeholder, bg_img=rand_img(), draft=True, editor=editor_type)
+    elif note_id is not None and editor_type is not None:
+        user_note = Note.query.filter_by(id=note_id).first_or_404()
+        if user_note.user_id != current_user.id:
+            abort(403)
+        if editor_type in ('simple', 'full'):
+            return render_template('note_editor.j2', note=user_note, bg_img=rand_img(), draft=False, editor=editor_type)
+    return render_template('note_editor.j2', note=user_note, bg_img=rand_img(), draft=False, editor='full')
